6f6bdddce2a223b2cb2f996d25d4da0976fc101c
Devanshu Kumar <31742441+devkumar07@users.noreply.github.com>
2019-05-21 16:00:33 -0700

    Update Matcher.java

--- Matcher.java
+++ Matcher.java
@@ -11,6 +11,8 @@ import java.io.Reader;
 import java.util.StringTokenizer;
 import java.util.Scanner;
 import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.List;
 public class Matcher{
   public int get_net_score(ArrayList<Student> roster, int student){
     int net_score=0;
@@ -18,5 +20,27 @@ public class Matcher{
         net_score+=roster.get(student).get_score(i);
     }
     return net_score;
+  }
+  //This method will sort the list by the preference that student puts
+  public ArrayList<Student> sort_by_preference(ArrayList<Student> roster, int project_id){
+    ArrayList<Student> final_list = new ArrayList<Student>();
+    ArrayList<Student> temp = roster;
+    int index = 0;
+    for(int i=0;i<temp.size();i++){
+      for(int j=i+1;j<temp.size();j++){
+          if(temp.get(j).get_score(project_id)>temp.get(i).get_score(project_id)){
+            index=j;
+          }
+      }
+      final_list.add(temp.get(index));
+      temp.remove(index);
+    }
+    return final_list;
+  }
+  public ArrayList<Student> sort_by_netscore(ArrayList<Student> roster){
+
+  }
+  public ArrayList<Student> match(ArrayList<Student> roster, int project){
+
   }
 }
