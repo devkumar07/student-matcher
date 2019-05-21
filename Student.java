b6c50bba1a3a72f031b87577704069393e8c241b
kiran <kiran@kiranraj.us>
2019-05-20 21:39:13 -0700

    aded net_score var in student.java

--- Student.java
+++ Student.java
@@ -5,20 +5,20 @@ public class Student {
   private String timestamp;
   private ArrayList<int[]> preferances = new ArrayList<int[]>();
 
-  public Student(String first, String last, String email, String time, ArrayList pref){
-      first_name = first;
-      last_name = last;
-      email_id = email;
-      timestamp = time;
-      preferances = pref;
-    }
+  public Student(String first, String last, String email, String time, ArrayList pref) {
+    first_name = first;
+    last_name = last;
+    email_id = email;
+    timestamp = time;
+    preferances = pref;
+  }
 
-  public Student(Student list){
-      this.first_name = list.first_name;
-      this.last_name = list.last_name;
-      this.email_id = list.email_id;
-      this.timestamp = list.timestamp;
-    }
+  public Student(Student list) {
+    this.first_name = list.first_name;
+    this.last_name = list.last_name;
+    this.email_id = list.email_id;
+    this.timestamp = list.timestamp;
+  }
 
   public String get_first_name() {
     return first_name;
@@ -35,8 +35,8 @@ public class Student {
   public String get_Time() {
     return timestamp;
   }
-  
-  public ArrayList get_preferances(){
+
+  public ArrayList get_preferances() {
     return preferances;
   }
 }
