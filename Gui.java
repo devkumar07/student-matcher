58e719b291af626c8d5b0a249a00a5e38ef34943
kiran <kiran@kiranraj.us>
2019-05-25 21:48:30 -0700

    gui fixed the string passthrough issue, needs to fix array out of bounds

--- Gui.java
+++ Gui.java
@@ -9,7 +9,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
-public class Gui {
+public class Gui implements ActionListener {
 
     JFrame frame;
     JPanel pan_input, pan_buttons;
@@ -30,30 +30,27 @@ public class Gui {
 
     public void launchFrame() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,150);
+        frame.setSize(300, 150);
 
         lbl_student_roster.setLabelFor(tf_student_roster);
         lbl_project_roster.setLabelFor(tf_project_roster);
 
         pan_input.setLayout(new SpringLayout());
- 
+
         pan_input.add(lbl_student_roster);
         pan_input.add(tf_student_roster);
         pan_input.add(lbl_project_roster);
         pan_input.add(tf_project_roster);
 
-        SpringUtilities.makeCompactGrid(pan_input,
-        2, 2,
-        6, 6,
-        6, 6);       
+        SpringUtilities.makeCompactGrid(pan_input, 2, 2, 6, 6, 6, 6);
 
         pan_buttons.add(but_match);
-       
+
         frame.getContentPane().add(pan_input, BorderLayout.PAGE_START);
         frame.getContentPane().add(pan_buttons, BorderLayout.PAGE_END);
         frame.setVisible(true);
 
-        but_match.addActionListener(new matchButton());
+        but_match.addActionListener(this);
 
         // b1.addActionListener(new ClearButton());
         // b2.addActionListener(new CopyButton());
@@ -61,12 +58,21 @@ public class Gui {
 
     public String getProjectFieldText() {
         return tf_project_roster.getText();
-      }
-      
+    }
+
     public String getStudentFieldText() {
         return tf_student_roster.getText();
-      }
+    }
 
+    public void actionPerformed(ActionEvent e) {
+
+        String student = this.getProjectFieldText(); // "roster.csv"; //instance.getProjectFieldText();
+        String projects = this.getStudentFieldText();// "projects.csv"; //instance.getStudentFieldText();
+
+        app.apprun(student, projects);
+
+        JOptionPane.showMessageDialog(null, "Done!");
+    }
 
     public static void main(String args[]) {
         Gui gui = new Gui();
@@ -74,16 +80,3 @@ public class Gui {
     }
 }
 
-class matchButton implements ActionListener {
-
-    public void actionPerformed(ActionEvent e) {
-
-        Gui instance = new Gui();
-        String student = "roster.csv";//instance.getProjectFieldText();
-        String projects = "projects.csv";//instance.getStudentFieldText();
-
-        app.apprun(student, projects);
-
-        JOptionPane.showMessageDialog(null, "Done!");
-    }
-}
